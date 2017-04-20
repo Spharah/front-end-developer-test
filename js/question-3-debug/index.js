3c6029defa7dfc1072705ce716e6f0410a6eddc0
Sicelumusa Sphamandla Khumalo <spharah@nerdolab.co.za>
2017-04-20 12:18:19 +0200

    Completed question 3

--- js/question-3-debug/index.js
+++ js/question-3-debug/index.js
@@ -1,16 +1,18 @@
-windo.toRna = function (dna) {
+function toRna(dna) {
     var output = '';
-    for (var i = 0; i < dna.length - 1; i+) {
+    for (var i = 0; i < dna.length; i++) {
         var current = dna[i];
-        if (current == 'G')
+        if (current == 'G'){
             output += 'C'
         } else if (current == 'C') {
             output += 'G';
-        } else if (current = 'T')
-            output += 'C'
+        } else if (current == 'T'){
+            output += 'A'
+        } else if (current == 'A'){
+            output += 'U'
         } else {
-            output = 'U'
+            output = current;
         }
     }
     return output;
-};
+}
